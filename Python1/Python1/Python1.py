c8d251331f1645103dc70c1153217af201096783
kwakkun2002 <36096367+kwakkun2002@users.noreply.github.com>
2018-03-10 14:31:48 +0900

    3/10 first class
    #문자열의 길이 변환
    #문자열의 길이 변환
    #연산자
    #괄호 (우선순위제공)
    #머리아픈 암산(우선순위)
    #그렇다면 문자열에서도 우선순위는?
    #문자열과 숫자를 더하면 타입에러가 난다.

--- Python1/Python1/Python1.py
+++ Python1/Python1/Python1.py
@@ -1,3 +1,9 @@
+#출력
+print("hello world")
+print(1+1,"hello")
+print('1'+1,"hello")
+
+#뒤에서 세기
 #print("안녕하세요"[-1])
 #print("안녕하세요"[-2])
 #print("안녕하세요"[-3])
@@ -10,7 +16,55 @@
 #print("안녕하세요"[1:5])
 #print("안녕하세요"[2:5])
 
-print("hello");
+#문자열의 길이 변환
+#print(len("안녕세상아"));
+
+#타입을 알자
+#print(type("안녕하세요"))
+#print(type(len("안녕하세요")))
+#print(12)
+#print(12.2)
+#print(type(12))
+#print(type(12.2))
+#print(0)
+#print(0.0)
+#print(type(0))
+#print(type(0.0))
+
+#연산자
+#print(5+7)
+#print(5-7)
+#print(5*7)
+#print(5/7)
+#print(3%2)
+#print(5//7)
+#print(2**1)
+#print(2**2)
+#print(2**3)
+
+#괄호 (우선순위제공)
+#print(1+1*2)
+#print((1+1)*2)
+
+#머리아픈 암산(우선순위)
+#print(2+2-2*2/2*2)
+#print(2-2+2/2*2+2)
+
+#그렇다면 문자연에서도 우선순위는?
+#print("안녕"+"하세요"*3)
+
+#문자열과 숫자를 더하면 타입에러가 난다.
+#string="문자열"
+#number=123
+#string+number
+
+
+
+
+
+
+
+
 
 
 
