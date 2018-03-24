def2e3bf73a43d408fdd7569571739895904373c
kwakkun2002 <36096367+kwakkun2002@users.noreply.github.com>
2018-03-24 14:31:48 +0900

    new file 3/24 learn if
    yee

--- Python1/Python1/Python1.py
+++ Python1/Python1/Python1.py
@@ -176,24 +176,55 @@
 #if num==0:
 #    print("0입니다")
 
-import datetime
+#import datetime
 
-now=datetime.datetime.now()
+#now=datetime.datetime.now()
 
-if now.hour < 12:
-    print("현재시간은 {}시로 오전입니다.".format(now.hour))
-if now.hour >= 12:
-    print("현재시간은 {}시로 오후입니다.".format(now.hour-12))
+#if now.hour < 12:
+#    print("현재시간은 {}시로 오전입니다.".format(now.hour))
+#if now.hour >= 12:
+#    print("현재시간은 {}시로 오후입니다.".format(now.hour-12))
 
-if 3<=now.month<=5:
-    print("현재시간은 {}월로 봄입니다.".format(now.month))
-if 6<=now.month<=8:
-    print("현재시간은 {}월로 여름입니다.".format(now.month))
-if 9<=now.month<=11:
-    print("현재시간은 {}월로 가을입니다.".format(now.month))
-if now.month==12 or 1<=now.month<=2:
-    print("현재시간은 {}월로 겨울입니다.".format(now.month))
-print()
+#if 3<=now.month<=5:
+#    print("현재시간은 {}월로 봄입니다.".format(now.month))
+#if 6<=now.month<=8:
+#    print("현재시간은 {}월로 여름입니다.".format(now.month))
+#if 9<=now.month<=11:
+#    print("현재시간은 {}월로 가을입니다.".format(now.month))
+#if now.month==12 or 1<=now.month<=2:
+#    print("현재시간은 {}월로 겨울입니다.".format(now.month))
+#print()
+
+
+#number=int(input("정수 입력 "))
+
+#if number%2==0:
+#    print("{}는 짝수 입니다.".format(number))
+#else:
+#     print("{}는 홀수 입니다.".format(number))
+
+#score=float(input("학점입력"))
+
+#if 4.5<=score:
+#    print("신")
+#elif 4.2<=score:
+#    print("교수님의 사랑")
+#elif 3.5<=score:
+#    print("현체재의 수호자")
+#elif 2.8<=score:
+#    print("일반인")
+#elif 2.3<=score:
+#    print("일탈을 꿈꾸는 소시민")
+#elif 1.75<=score:
+#    print("오락문화의 선구자")
+#elif 1.0<=score:
+#    print("불가촉천민")
+#elif 0.5<=score:
+#    print("자벌레")
+#elif 0<=score:
+#    print("플랑크톤")
+#else:
+#    print("시대를 앞서 가는 혁명의 씨앗")
 
    
 
